8a03da4b2d9a2a6d06d4174989ea79459bd1f758
Caio Andrade <caio.andrade.1705@gmail.com>
2022-10-06 15:12:14 -0300

    Layout da página de Registro

--- lib/app/modules/auth/register/register_page.dart
+++ lib/app/modules/auth/register/register_page.dart
@@ -1,15 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_list_provider/app/core/interface/theme_extension.dart';
+import 'package:flutter_todo_list_provider/app/core/widget/todo_list_field.dart';
+import 'package:flutter_todo_list_provider/app/core/widget/todo_list_logo.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Todo List',
+              style: TextStyle(
+                fontSize: 10,
+                color: context.primaryColor,
+              ),
+            ),
+            Text(
+              'Cadastro',
+              style: TextStyle(
+                fontSize: 15,
+                color: context.primaryColor,
+              ),
+            ),
+          ],
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: ClipOval(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: context.primaryColor.withAlpha(20),
+              ),
+              child: Icon(
+                Icons.arrow_back_ios_outlined,
+                size: 20,
+                color: context.primaryColor,
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Container(),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: screenWidth * .5,
+            child: const FittedBox(
+              fit: BoxFit.fitHeight,
+              child: TodoListLogo(),
+            ),
+          ),
+          Form(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 20,
+              ),
+              child: Column(
+                children: [
+                  TodoListField(label: 'E-mail'),
+                  const SizedBox(height: 20),
+                  TodoListField(
+                    label: 'Senha',
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  TodoListField(
+                    label: 'Confirmar senha',
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text('Cadastrar'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
